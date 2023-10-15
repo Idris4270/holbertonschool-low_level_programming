1e9b0ea4ed8227c83a5f5b5233b85f206d8f2b6f
Idris Y <idrisrosario@gmail.com>
2023-10-15 00:45:21 -0400

    5-printf this time4

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string sing the printf command
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar. but the outcome is a peice of art,\n";
+	return (0);
+}
