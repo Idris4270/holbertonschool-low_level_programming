684101829b30add275e1639d242b2ecebe582eb7
Idris Y <idrisrosario@gmail.com>
2023-10-15 01:15:30 -0400

    6-printf times 5

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/** - prints the size of multiple data types
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
