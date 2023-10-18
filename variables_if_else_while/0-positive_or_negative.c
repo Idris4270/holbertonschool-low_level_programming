2eefddda9fb922a6f31be1d0ad00377ffd2d2a68
Idris Y <idrisrosario@gmail.com>
2023-10-18 12:03:20 -0400

    0-if else nightmare

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - give a random number to variable n and say if it's more, less or equal to 0
+ * return 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0){
+		printf("%d is positive",n);
+	}
+	else if(n < 0){
+		printf("%d is negative",n);
+	}
+	else if(n == 0){
+		printf("%d is zero",n);
+	}
+
+	return (0);
+}
