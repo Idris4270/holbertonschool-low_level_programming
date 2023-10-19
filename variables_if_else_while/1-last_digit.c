6394f919555269c1fdaa08e370c6de797d1b85a1
Idris Y <idrisrosario@gmail.com>
2023-10-19 10:50:37 -0400

    1-cool code that takes a while without copy paste...

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if(u > 5)
 	{
-		printf("Last digit of %d is %f and is greater than 5\n", n u);
+		printf("Last digit of %d is %f and is greater than 5\n", n, u);
 	}
 	else if(u == 0)
 	{
-		printf("Last digit of %d is %f and is 0\n", n u);
+		printf("Last digit of %d is %f and is 0\n", n, u);
 	}
 	else(u < 6)
 	{
-		printf("Last digit of %d is %f and is less than 6 and not 0\n", n u);
+		printf("Last digit of %d is %f and is less than 6 and not 0\n", n, u);
 	}
 		return (0);
 	}
