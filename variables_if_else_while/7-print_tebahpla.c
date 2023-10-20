503218aa18c22175f279868e9ff6c25721f83119
Idris Y <idrisrosario@gmail.com>
2023-10-20 00:19:30 -0400

    7-legit copy pasting number 2 and switching a and z around

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-  2 /**
-  3  * main - entry point
-  4  * Return: always 0 (Success)
-  5  */
-  6 
-  7 int main(void)
-  8 {
-  9         char c;
- 10 
- 11         c = 'z';
- 12 
- 13         while (c <= 'a')
- 14         {
- 15                 putchar (c);
- 16                 c = c + 1;
- 17         }
- 18         putchar ('\n');
- 19 
- 20         return (0);
- 21 }
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+ 
+ int main(void)
+ {
+         char c;
+ 
+         c = 'z';
+ 
+         while (c <= 'a')
+         {
+                 putchar (c);
+                 c = c + 1;
+         }
+         putchar ('\n');
+ 
+         return (0);
+ }
