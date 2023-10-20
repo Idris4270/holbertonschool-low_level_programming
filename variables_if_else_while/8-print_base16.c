f2f1d4ae023243168cb0560e10552733e0bef31e
Idris Y <idrisrosario@gmail.com>
2023-10-20 08:34:08 -0400

    8-using the ascii list is annoying

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	char b;
+
+	a = 48;
+	b = 97;
+
+	while (a <= 57)
+	{
+		putchar (a);
+		a++;
+	}
+	while (b <= 102)
+	{
+		putchar (b);
+		b++;
+	}
+	putchar ('\n');
+	return (0);
+}
