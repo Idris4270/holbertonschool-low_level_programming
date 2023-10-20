46297546ca0af5172ec43788bbf3ca091678ef3f
Idris Y <idrisrosario@gmail.com>
2023-10-19 21:59:28 -0400

    4-attempt 1

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+		if (c != 'e' && c != 'q')
+		{
+			putchar (c);
+			c++;
+		}
+		else
+		{
+			c++;
+		}
+	return (0);
+}
