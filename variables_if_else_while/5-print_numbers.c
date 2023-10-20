532a5b7ec51d080333639e9cffcd8287a32e9912
Idris Y <idrisrosario@gmail.com>
2023-10-19 22:12:30 -0400

    5-using for with putchar

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = '1'
+
+	for (ch = '1'; ch <= '9'; ch++)
+		putchar(ch);
+
+	return (0);
+}
