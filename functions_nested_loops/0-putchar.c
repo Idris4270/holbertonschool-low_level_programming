4739f4fb64d51760f25f221f9ddad18bfb2ad34b
Idris Y <idrisrosario@gmail.com>
2023-10-20 23:13:49 -0400

    0-printing putchar with putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char *c;
+	int i;
+
+	c = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar (c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
